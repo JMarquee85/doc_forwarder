2e1b9b487ce3a05515f0c4075fc8577fc43a42a9
Josh Marcus <joshmarcus85@gmail.com>
2017-06-02 11:50:11 -0400

    Cleaning things up a bit in this branch.

--- email_it.py
+++ email_it.py
@@ -2,13 +2,13 @@
 ## This function checks to see if the customer's registration documents
 ## have already been emailed to info@pupculturenyc.com by checking 
 ## maillog.txt.
-import sys
-sys.path.append("/home/josh/Documents/python_work/doc_forwarder") 
+#import sys
+#sys.path.append("/home/josh/Documents/python_work/doc_forwarder") 
 import os
 import gspread
 import smtplib
 import dropbox
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 from slacker import Slacker
 import get_reg as gr
 
@@ -23,6 +23,9 @@ from email import encoders
 
 ### IMPORT PRIVATE CONFIG FILE
 import private_config as p_con
+
+## Circular imports here, I realize ...
+import create_docx as c_doc
 
 from datetime import datetime
 
@@ -40,27 +43,36 @@ today_date = now.strftime("%m/%d/%Y %I:%M")
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 ##### JSON Key #####
-json_key = os.path.join(this_dir, 'pcregisterpdfforwarder-1f39ce9a4ac0.json')
+#json_key = os.path.join(this_dir, 'pcregisterpdfforwarder-1f39ce9a4ac0.json')
 
 def email_it(row_number):
-	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-	credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key, scope)
-	gc = gspread.authorize(credentials)
+
+	### Instead of opening the Google Sheet here, you should call
+	### the established variables from create_docx here if possible. 
+	
+	#scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	#credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key, scope)
+	#gc = gspread.authorize(credentials)
 	
 	##### EMAIL PUPCULTURE FUNCTION #####	
 	# See http://naelshiab.com/tutorial-send-email-python/
-	reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO9t9rzIaTijrbLQBCNYbulAuF4/edit#gid=136975089')
-	worksheet = reg.get_worksheet(0)
+	#reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO9t9rzIaTijrbLQBCNYbulAuF4/edit#gid=136975089')
+	#worksheet = reg.get_worksheet(0)
 	
-	last_name = worksheet.acell('B' + str(row_number)).value
-	first_name = worksheet.acell('C' + str(row_number)).value
-	pet_name = worksheet.acell('R' + str(row_number)).value
+	#last_name = worksheet.acell('B' + str(row_number)).value.strip()
+	#first_name = worksheet.acell('C' + str(row_number)).value.strip()
+	#pet_name = worksheet.acell('R' + str(row_number)).value.strip()
+	
+	### TRYING THIS OUT ###
+	last_name = c_doc.worksheet.acell('B' + str(row_number)).value.strip()
+	first_name = c_doc.worksheet.acell('C' + str(row_number)).value.strip()
+	pet_name = c_doc.worksheet.acell('R' + str(row_number)).value.strip()
 	
 	### MESSAGE TO ANNOUNCE EMAILING DOCUMENT
-	print("\nEmailing a registration document from " + pet_name.upper().strip() + " " + 
-			last_name.upper().strip() + " to " + p_con.recipient_email + "!")
+	print("\nEmailing a registration document from " + pet_name.title() + " " + 
+			last_name.title() + " to " + p_con.recipient_email + "!")
 		### Post Status Message to Slack Channel ###
-	new_email_slack_msg = ('\nA new registration document for ' + pet_name.upper().strip() + ' ' + last_name.upper().strip() + ' has been emailed!')
+	new_email_slack_msg = ('\nA new registration document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
 	slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
 	
 	################
@@ -69,19 +81,19 @@ def email_it(row_number):
 	'''
 	print("\nEMAIL CUSTOMER REGISTRATION:")
 	print("\nChecking to see if we have documents for " + 
-				pet_name.upper() + " " + last_name.upper() +
+				pet_name.title() + " " + last_name.title() +
 				" already sent ...")	
 	
 	mail_log = open('maillog.txt', 'r')
 	
-	if last_name.upper() and pet_name.upper() in mail_log.read():
+	if last_name.title() and pet_name.title() in mail_log.read():
 		print("\nThis customer's documents have already been emailed to pupculture! Moving on ... \n")
 		pass
 	else:
-		print("\nEmailing a registration document from " + pet_name.upper() + " " + 
-			last_name.upper() + " to " + p_con.recipient_email + "!")
+		print("\nEmailing a registration document from " + pet_name.title() + " " + 
+			last_name.title() + " to " + p_con.recipient_email + "!")
 		### Post Status Message to Slack Channel ###
-		new_email_slack_msg = ('\nA new registation document for ' + pet_name.upper() + ' ' + last_name.upper() + ' has been emailed!')
+		new_email_slack_msg = ('\nA new registation document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
 		slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
 	'''	
 	#################
@@ -95,19 +107,19 @@ def email_it(row_number):
 	server.ehlo()
 	server.login(p_con.serv_email_address, p_con.serv_email_password)
 	msg = MIMEMultipart()
-	msg['Subject'] = "New Registration from " + pet_name.upper().strip() + ' ' + last_name.upper().strip() + "!"
+	msg['Subject'] = "New Registration from " + pet_name.title() + ' ' + last_name.title() + "!"
 	msg['From'] = me
 	msg['To'] = you
  
-	body = first_name.upper() + " " + last_name.upper().strip() + " has registered their dog " + pet_name.upper().strip() + " with pupculture!\n\n Please Note: uploaded customer vaccinations and dog images are uploaded to the Dropbox at http://pupculturenyc.com/upload"
+	body = first_name.title().strip() + " " + last_name.title() + " has registered their dog " + pet_name.title() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n"
 	
 	msg.attach(MIMEText(body, 'plain'))
 	
 	### Attach completed customer file ###
 		
-	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.upper().strip() + ', ' + pet_name.upper().strip() + ".docx")
+	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.title().strip() + ', ' + pet_name.title().strip() + ".docx")
 	os.chdir('submitted_customer_files')
-	cust_filename = open(last_name.upper().strip() + ', ' + pet_name.upper().strip() + ".docx", "rb")
+	cust_filename = open(last_name.title().strip() + ', ' + pet_name.title().strip() + ".docx", "rb")
 	attachment = cust_filename
 	
 	part = MIMEBase('application', 'octet-stream')
@@ -133,7 +145,7 @@ def email_it(row_number):
 	# If in file, don't print message or send email	
 		
 	with open('maillog.txt', 'a') as mail_log:
-		mail_log.write(last_name.upper().strip() + ', ' + pet_name.upper().strip() + ': ' + str(today_date) + "\n")
+		mail_log.write(last_name.title().strip() + ', ' + pet_name.title().strip() + ': ' + str(today_date) + " --MANUAL REPRINT\n")
 		
 	'''
 	##### Saving to Dropbox #####
@@ -144,14 +156,14 @@ def email_it(row_number):
 	os.chdir('submitted_customer_files')
 				
 	f = open(cust_filename, 'rb')
-	response = client.put_file(('/' + last_name.upper() + ', ' + pet_name.upper() + ".docx"), 'f') 
+	response = client.put_file(('/' + last_name.title() + ', ' + pet_name.title() + ".docx"), 'f') 
 	print 'uploaded ', response
 		
 	folder_metadata = client.metadata('/')
 	print 'metadata: ', folder_metadata
 		
-	f, metadata = client.get_file_and_metadata('/' + last_name.upper() + ', ' + pet_name.upper() + ".docx")
-	out = open('/' + last_name.upper() + ', ' + pet_name.upper() + ".docx", 'wb')
+	f, metadata = client.get_file_and_metadata('/' + last_name.title() + ', ' + pet_name.title() + ".docx")
+	out = open('/' + last_name.title() + ', ' + pet_name.title() + ".docx", 'wb')
 	out.write(f.read())
 	out.close()
 	print metadata
--- missed_one.py
+++ missed_one.py
@@ -5,12 +5,10 @@
 
 # Look into option to select from a list
 # with that ncurses library I ran accross
-
 from __future__ import print_function
-from requests.exceptions import ConnectionError
-
+#from requests.exceptions import ConnectionError
 ##### MAIN IMPORT STATEMENTS #####
-from mailmerge import MailMerge
+#from mailmerge import MailMerge
 from datetime import datetime
 import dropbox
 # See https://pypi.python.org/pypi/dropbox for more information
@@ -19,21 +17,20 @@ import dropbox
 ### Private configuration file
 import private_config as p_con
 ### Email PupCulture Function
-import email_pupculture as e_pc
+#import email_pupculture as e_pc
 ### Get Registration Function
-import get_reg as gr
+#import get_reg as gr
 import email_it as e_it
 import create_docx as c_doc
 
-import gspread
-import json
+#import gspread
+#import json
 import sys
 import os
-import smtplib
+#import smtplib
 import csv
-import socket
-import urllib, re
-from oauth2client.service_account import ServiceAccountCredentials
+#import socket
+#import urllib, re
 
 ### SLACK CLIENT ###
 from slacker import Slacker
@@ -46,35 +43,6 @@ today_date = now.strftime("%m/%d/%Y %I:%M")
 # Creating relative path directory
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
-# JSON Credentials and Scope 
-
-##### JSON Key #####
-json_key = os.path.join(this_dir, 'pcregisterpdfforwarder-1f39ce9a4ac0.json')
-
-##### SCOPE #####
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-# Google Credentials
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key, scope)
-global gc
-gc = gspread.authorize(credentials)
-
-# Open Registration Google Spreadsheet
-reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO9t9rzIaTijrbLQBCNYbulAuF4/edit#gid=136975089')
-worksheet = reg.get_worksheet(0)
-print("Google sheet successfully opened!")
-
-##### 
-# For more information on using MailMerge:
-# pbpython.com/python-word-template.html
-# Using templates with docx-mailmerge
-
-# Template File
-template = os.path.join(this_dir, 'pcregtemplate.docx')
-
-global last_name
-global first_name
-global pet_name
-
 #####  THE FUNCTION #####
 ###
 #
@@ -83,113 +51,13 @@ def reprint_row(row_select):
 	# Check to make sure the input represents
 	# a row with customer content in it
 	if row_select <= 1:
-		print("\n\nPlease input a number of 2 or higher! (Note: 2 is the first row in the Google Doc.)")
-		return
+		print("\n*** INVALID ENTRY ***\nPlease input a number of 2 or higher! \n(Note: 2 is the first row in the Google Doc.)")
+		#return
 	else:
 		print("\nImporting customer information from Row " + 
 				str(row_select) + "...")
-		# Input rowselect number to call particular row
-		time_registered = worksheet.acell('A' + str(row_select)).value
-		last_name = worksheet.acell('B' + str(row_select)).value
-		first_name = worksheet.acell('C' + str(row_select)).value
-		street = worksheet.acell('D' + str(row_select)).value
-		apt = worksheet.acell('E' + str(row_select)).value
-		cross_streets = worksheet.acell('F' + str(row_select)).value
-		city = worksheet.acell('G' + str(row_select)).value
-		state = worksheet.acell('H' + str(row_select)).value
-		zip_code = worksheet.acell('I' + str(row_select)).value
-		home_phone = worksheet.acell('J' + str(row_select)).value
-		cell_phone = worksheet.acell('K' + str(row_select)).value
-		business_phone = worksheet.acell('L' + str(row_select)).value
-		email_address = worksheet.acell('M' + str(row_select)).value
-		# Where did you hear about us?
-		reference = worksheet.acell('N' + str(row_select)).value
-		emergency_contacts = worksheet.acell('O' + str(row_select)).value
-		membership = worksheet.acell('P' + str(row_select)).value
-		keys = worksheet.acell('Q' + str(row_select)).value
-		# Pet Information Section
-		pet_name = worksheet.acell('R' + str(row_select)).value
-		pet_nick = worksheet.acell('S' + str(row_select)).value
-		breed = worksheet.acell('T' + str(row_select)).value
-		weight = worksheet.acell('U' + str(row_select)).value
-		sex = worksheet.acell('V' + str(row_select)).value
-		dob = worksheet.acell('W' + str(row_select)).value
-		color = worksheet.acell('X' + str(row_select)).value
-		fixed = worksheet.acell('Y' + str(row_select)).value
-		# How long have you owned your dog?
-		length_owned = worksheet.acell('Z' + str(row_select)).value
-		# Please specify what brand and type (wet/ dry) food you use:
-		brand_food = worksheet.acell('AA' + str(row_select)).value
-		# How many times per day do you feed your dog?
-		times_per_day_feed = worksheet.acell('AB' + str(row_select)).value
-		# What size serving?
-		serving_size = worksheet.acell('AC' + str(row_select)).value
-		# Does your dog have any allergies or digestive problems?
-		allergies_digestive = worksheet.acell('AD' + str(row_select)).value
-		# Is your dog allowed to have treats?
-		treats_ok = worksheet.acell('AE' + str(row_select)).value
-		# Should your dog be fed during daycare?
-		fed_during_daycare = worksheet.acell('AF' + str(row_select)).value
-		# IN YOUR HOME SECTION
-		# Is your dog restricted to a certain area of your home?
-		home_restriction = worksheet.acell('AG' + str(row_select)).value
-		# Do you leave water out all the time?
-		water_out = worksheet.acell('AH' + str(row_select)).value
-		# Dry food?
-		dry_food = worksheet.acell('AI' + str(row_select)).value
-		# Where do you keep the leash and collar/harness?
-		leash_location = worksheet.acell('AJ' + str(row_select)).value
-		# Where do you keep dog food, treats, and feeding/water dishes?
-		where_is_the_stuff = worksheet.acell('AK' + str(row_select)).value
-		# Where do you keep other items we might need (i.e. wee-wee pads, paper towels, toys)?
-		where_is_the_other_stuff = worksheet.acell('AL' + str(row_select)).value
-		# Do you have any instructions regarding lights and locks?
-		lights_locks = worksheet.acell('AM' + str(row_select)).value
-		# Do you have any other helpful information such as hiding spots or personality quirks?
-		helpful_info = worksheet.acell('AN' + str(row_select)).value
-		# Please describe where your dog is allowed in your home.
-		dog_allowed = worksheet.acell('AO' + str(row_select)).value
-		# Please describe your dog's behavior around other dogs.
-		behavior = worksheet.acell('AP' + str(row_select)).value
-		# Please describe your dog's behavior around new people:
-		behavior_new_people = worksheet.acell('AQ' + str(row_select)).value
-		# Please describe your dog's behavior on a leash.
-		behavior_leash = worksheet.acell('AR' + str(row_select)).value
-		# Is your dog housebroken?
-		housebroken = worksheet.acell('AS' + str(row_select)).value
-		# When was you last visit to the vet?
-		last_vet = worksheet.acell('AT' + str(row_select)).value
-		# What type of flea prevention do you use?
-		flea_prevention = worksheet.acell('AU' + str(row_select)).value
-		# Does your dog have any medical conditions?
-		medical_conditions = worksheet.acell('AV' + str(row_select)).value
-		# Does your dog require any medications?
-		medications = worksheet.acell('AW' + str(row_select)).value
-		# Has your dog ever bitten or tried to attack another dog or human being? (If yes, please elaborate below.) 
-		ever_attacked = worksheet.acell('AX' + str(row_select)).value
-		# Please list your dog's medications. 
-		medication_list = worksheet.acell('AY' + str(row_select)).value
-		# Please describe your dog's medication conditions.
-		describe_medical_conditions =  worksheet.acell('AZ' + str(row_select)).value
-		#VETERINARIAN SECTION
-		vet_name = worksheet.acell('BA' + str(row_select)).value
-		vet_phone = worksheet.acell('BB' + str(row_select)).value
-		vet_address = worksheet.acell('BC' + str(row_select)).value
-		vet_city = worksheet.acell('BD' + str(row_select)).value
-		vet_state = worksheet.acell('BE' + str(row_select)).value
-		# Do you agree with the terms?
-		terms_agreement = worksheet.acell('BF' + str(row_select)).value
-		# Receives another email address here for some reason. 
-		email_again = worksheet.acell('BG' + str(row_select)).value
-		
-		# Variables changed to selected row. 
-		# Printing confirmation message. 
-		print("Creating docx file for "  + pet_name.upper().strip() + " " +
-					last_name.upper().strip() + "...")
-		
-		
-		
-		
+		c_doc.create_docx(row_select)
+		e_it.email_it(row_select)
 
 ##### DO THE BIT! #####
 
@@ -197,22 +65,24 @@ while True:
 	# User inputs number to select row to reprint
 	try:
 		# Accepts user input by row number
-		row_select = input("Please input the row number you would like to reprint and resend ... \n\t")
+		print("\n\n\n\n\t\t***** PUPCULTURE REGISTRATION FORWARDER *****")
+		print("\n\t\t--Manual Reprint by Google Sheet Row Number--")
+		row_select = input("\nINPUT ROW NUMBER ... (2 or higher) \n\t")
 		reprint_row(int(row_select))
-		# Create docx file			
-		c_doc.create_docx(row_select)
-		print("\nFile created!")
-		##### EMAILING DOCX FILE
-		print("\nEmailing docx file!\n")
 		# Email file to pupculture
-		e_it.email_it(row_select)	
+		#e_it.email_it(row_select)	
 	except KeyboardInterrupt:
 		print("\nOK! Exiting program!")
-		break
-	except ConnectionError:
-		print("\nUnable to connect! Please ensure you are connected to the internet! \nTrying again!")
-		slack.chat.post_message(p_con.slack_channel, 'Connection lost! Attempting to reconnect ... ')
-		
+		break.
+	#except ConnectionError:
+	#	print("\nUnable to connect! Please ensure you are connected to the internet! \nTrying again!")
+	#	slack.chat.post_message(p_con.slack_channel, 'Connection lost! Attempting to reconnect ... ')
+	except SyntaxError:
+		print("\n\tThat didn't seem to work! Try again.\n")
+	except IOError:
+		print("\nFile writing error! Please make sure that you input a value of 2 or higher ...")
+	except NameError:
+		print("\n\tUhhhhh.... what?! Let's try that again.")
 	
 
 	
