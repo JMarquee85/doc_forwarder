707aa63b75d6049bdf22b8274bf2dc49aa3e8a75
Josh Marcus <joshmarcus85@gmail.com>
2017-06-05 12:09:10 -0400

    A working commit. missed_one should be relativelt functional

--- email_it.py
+++ email_it.py
@@ -1,29 +1,14 @@
-#import main_loop
-## This function checks to see if the customer's registration documents
-## have already been emailed to info@pupculturenyc.com by checking 
-## maillog.txt.
-#import sys
-#sys.path.append("/home/josh/Documents/python_work/doc_forwarder") 
+##### THIS FUNCTION ACCEPTS A NUMBER FROM missed_one 
+##### THAT MATCHES A ROW IN THE GOOGLE SHEET.
+### IT WILL REPRINT AND RESEND THE DOCX FILE IF THE 
+### 24/7 SCRIPT WAS NOT RUNNING OR MISSED ONE
+### TO RUN THIS SCRIPT, RUN missed_one.py
+
 import os
 import gspread
 import smtplib
 import dropbox
-#from oauth2client.service_account import ServiceAccountCredentials
 from slacker import Slacker
-#import get_reg as gr
-
-'''
-# EMAIL SECTION IMPORTS
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
-from email.mime.application import MIMEApplication
-from email.utils import formatdate
-from email.utils import make_msgid
-from email import encoders
-'''
-
-
 
 ### IMPORT PRIVATE CONFIG FILE
 import private_config as p_con
@@ -42,7 +27,7 @@ import yagmail
 #import missed_one as mo
 
 ##### SLACK TOKEN #####
-slack = Slacker(p_con.private_slack_token)
+#slack = Slacker(p_con.private_slack_token)
 
 # Pulling Today's Date
 now = datetime.now()
@@ -56,46 +41,27 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 
 def email_it(row_number):
 
-	yag = yagmail.SMTP('pupcultureforwarder', p_con.serv_email_password)
-
-	### Instead of opening the Google Sheet here, you should call
-	### the established variables from create_docx here if possible. 
+	# Initialize yagmail as yag
+	yag = yagmail.SMTP(p_con.serv_email_address, p_con.serv_email_password)
 	
-	#scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-	#credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key, scope)
-	#gc = gspread.authorize(credentials)
-	
-	##### EMAIL PUPCULTURE FUNCTION #####	
-	# See http://naelshiab.com/tutorial-send-email-python/
-	#reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO9t9rzIaTijrbLQBCNYbulAuF4/edit#gid=136975089')
-	#worksheet = reg.get_worksheet(0)
-	
-	#last_name = worksheet.acell('B' + str(row_number)).value.strip()
-	#first_name = worksheet.acell('C' + str(row_number)).value.strip()
-	#pet_name = worksheet.acell('R' + str(row_number)).value.strip()
-	
-	### TRYING THIS OUT ###
 	last_name = c_doc.worksheet.acell('B' + str(row_number)).value.strip()
 	first_name = c_doc.worksheet.acell('C' + str(row_number)).value.strip()
 	pet_name = c_doc.worksheet.acell('R' + str(row_number)).value.strip()
+	banner_img = 'pcemailbanner.png'
+	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.title().strip() + '_' + pet_name.title().strip() + ".docx")
 	
-	### YAGMAIL CONTENTS
 	
-	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.title().strip() + ', ' + pet_name.title().strip() + ".docx")
-	
-	subject = ("New Registration from " + pet_name.title() + ' ' + last_name.title() + "!")
-	# Not sure if I formatted this section correctly. 
-	contents = [first_name.title().strip() + " " + last_name.title() + " has registered their dog " + pet_name.title() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n", filename]
-	
+	### SEND THE EMAIL
+	subject = ("New Registration from " + pet_name.title().strip() + ' ' + last_name.title().strip() + "!")
+	contents = [banner_img, first_name.title().strip() + " " + last_name.title().strip() + " has registered their dog " + pet_name.title().strip() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n", filename]
 	yag.send(p_con.recipient_email, subject, contents)
-	
 	
 	### MESSAGE TO ANNOUNCE EMAILING DOCUMENT
 	print("\nEmailing a registration document from " + pet_name.title() + " " + 
 			last_name.title() + " to " + p_con.recipient_email + "!")
 		### Post Status Message to Slack Channel ###
 	new_email_slack_msg = ('\nA new registration document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
-	slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
+	c_doc.slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
 	
 	################
 	# Skipping the log check, as this function is for manual 
@@ -119,57 +85,7 @@ def email_it(row_number):
 		slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
 	'''	
 	#################
-	'''
-	me = p_con.serv_email_address	# Sender
-	you = p_con.recipient_email  # Recipient
-		
-	server = smtplib.SMTP(p_con.smtp_server, p_con.smtp_port)
-	server.ehlo()
-	server.starttls()
-	server.ehlo()
-	server.login(p_con.serv_email_address, p_con.serv_email_password)
-	msg = MIMEMultipart()
-	msg['Subject'] = "New Registration from " + pet_name.title() + ' ' + last_name.title() + "!"
-	msg['From'] = me
-	msg['To'] = you
- 
-	body = first_name.title().strip() + " " + last_name.title() + " has registered their dog " + pet_name.title() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n"
 	
-	msg.attach(MIMEText(body, 'plain'))
-	
-	### Attach completed customer file ###
-		
-	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.title().strip() + ', ' + pet_name.title().strip() + ".docx")
-	os.chdir('submitted_customer_files')
-	cust_filename = open(last_name.title().strip() + ', ' + pet_name.title().strip() + ".docx", "rb")
-	attachment = cust_filename
-	
-	part = MIMEBase('application', 'octet-stream')
-	part.set_payload((attachment).read())
-	encoders.encode_base64(part)
-	part.add_header('Content-Disposition', "attachment; filename= %s" % os.path.basename(filename))
-
-	# Have noticed a bug here if there either is a 
-	# document already with the filename or already listed
-	# in the log files. It kills the program here if that condition
-	# is happening. 
-		
-	msg.attach(part)
-	
-	text = msg.as_string()
-	server.sendmail(me, you, text)
-	server.quit()
-		
-	# Change directory back to program root dir
-	os.chdir('..')
-		
-	# Log the email to a text or csv file
-	# If in file, don't print message or send email	
-		
-	with open('maillog.txt', 'a') as mail_log:
-		mail_log.write(last_name.title().strip() + ', ' + pet_name.title().strip() + ': ' + str(today_date) + " --MANUAL REPRINT\n")
-		
-	'''
 	
 	#############################################
 	
--- missed_one.py
+++ missed_one.py
@@ -4,16 +4,12 @@
 # have missed
 
 # Look into option to select from a list
-# with that ncurses library I ran accross
+# with that ncurses library I ran across
+
 from __future__ import print_function
-#from requests.exceptions import ConnectionError
 ##### MAIN IMPORT STATEMENTS #####
 #from mailmerge import MailMerge
 from datetime import datetime
-#import dropbox
-import sys
-import os
-
 # See https://pypi.python.org/pypi/dropbox for more information
 
 ##### MY IMPORTS #####
@@ -21,7 +17,14 @@ import os
 import private_config as p_con
 import email_it as e_it
 import create_docx as c_doc
-#import db_upload as d
+import import_gsheet_by_row as ig
+
+import sys
+import os
+#import smtplib
+import csv
+#import socket
+#import urllib, re
 
 ### SLACK CLIENT ###
 from slacker import Slacker
@@ -34,24 +37,6 @@ today_date = now.strftime("%m/%d/%Y %I:%M")
 # Creating relative path directory
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
-#####  THE FUNCTION #####
-###
-#
-
-def reprint_row(row_select):
-	# Check to make sure the input represents
-	# a row with customer content in it
-	if row_select <= 1:
-		print("\n*** INVALID ENTRY ***\nPlease input a number of 2 or higher! \n(Note: 2 is the first row in the Google Doc.)")
-		#return
-	else:
-		print("\nImporting customer information from Row " + 
-				str(row_select) + "...")
-		c_doc.create_docx(row_select)
-		#d.db_upload(row_select)
-		e_it.email_it(row_select)
-		
-
 ##### DO THE BIT! #####
 
 while True:
@@ -61,7 +46,15 @@ while True:
 		print("\n\n\n\n\t\t***** PUPCULTURE REGISTRATION FORWARDER *****")
 		print("\n\t\t--Manual Reprint by Google Sheet Row Number--")
 		row_select = input("\nINPUT ROW NUMBER ... (2 or higher) \n\t")
-		reprint_row(int(row_select))
+		if int(row_select) <= 1:
+			print("\n*** INVALID ENTRY ***\nPlease input a number of 2 or higher! \n(Note: 2 is the first row in the Google Doc.)")
+			#return
+		else:
+			print("\nImporting customer information from Row " + 
+					str(row_select) + "...")
+			ig.import_gsheet_by_row(row_select)
+			#d.db_upload(row_select)
+			e_it.email_it(row_select)				
 	except KeyboardInterrupt:
 		print("\nOK! Exiting program!")
 		break
@@ -72,10 +65,8 @@ while True:
 		print("\n\tThat didn't seem to work! Try again.\n")
 	except IOError:
 		print("\nFile writing error! Please make sure that you input a value of 2 or higher ...")
-	#except NameError:
-	#	print("\n\tUhhhhh.... what?! Let's try that again.")
-	except KeyboardInterrupt:
-		print("\nAlright, we'll try again soon, then ...")
+	except NameError:
+		print("\n\tUhhhhh.... what?! Let's try that again.")
 	
 
 	
