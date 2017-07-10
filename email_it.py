b0eea40293c2b4b812f20dbf1ae366abccd15a97
Josh <josh@debian>
2017-07-10 18:06:21 +0000

    A big fix and some structural changes. Files should be written correctly now and I parsed down a couple of the functions. Add a clear all responses option with a master password requirement? Need to take out the Slack messaging and trade this for logs as well.

--- email_it.py
+++ email_it.py
@@ -68,14 +68,6 @@ def email_it(row_number):
 	contents = ['pcemailbanner.png', first_name.title().strip() + " " + last_name.title().strip() + " has registered their dog " + pet_name.title().strip() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n", filename]
 	yag.send(p_con.recipient_emails, subject, contents)
 	
-	### MESSAGE TO ANNOUNCE EMAILING DOCUMENT
-	print("\nEmailing a registration document from " + pet_name.title() + " " + 
-			last_name.title() + " to " + p_con.recipient_emails + "!")
-		### Post Status Message to Slack Channel ###
-	new_email_slack_msg = ('\nA new registration document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
-	slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
-	
-	
 	print("\nChecking to see if we have documents for " + 
 				pet_name.title() + " " + last_name.title() +
 				" already sent ...")	
@@ -87,11 +79,44 @@ def email_it(row_number):
 		pass
 	else:
 		print("\nEmailing a registration document from " + pet_name.title() + " " + 
-			last_name.title() + " to " + p_con.recipient_email + "!")
+			last_name.title() + " to " + p_con.recipient_emails + "!")
 		### Post Status Message to Slack Channel ###
 		new_email_slack_msg = ('\nA new registation document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
 		slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
 		return
+		
+def email_it_no_logs(row_number):
+	scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(json_key, scope)
+	gc = gspread.authorize(credentials)
+	reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO9t9rzIaTijrbLQBCNYbulAuF4/edit#gid=136975089')
+	global worksheet
+	worksheet = reg.get_worksheet(0)
+
+	# Initialize yagmail as yag
+	yag = yagmail.SMTP(p_con.serv_email_address, p_con.serv_email_password)
+	
+	last_name = worksheet.acell('B' + str(row_number)).value.strip()
+	first_name = worksheet.acell('C' + str(row_number)).value.strip()
+	pet_name = worksheet.acell('R' + str(row_number)).value.strip()
+	
+	filename = os.path.join(this_dir, "submitted_customer_files/" + last_name.title().strip() + '_' + pet_name.title().strip() + ".docx")
+	banner_img = 'pcemailbanner.png'
+	
+	### SEND THE EMAIL
+	subject = ("New Registration from " + pet_name.title().strip() + ' ' + last_name.title().strip() + "!")
+	contents = ['pcemailbanner.png', first_name.title().strip() + " " + last_name.title().strip() + " has registered their dog " + pet_name.title().strip() + " with pupculture!\nThe registration form is attached to this email. \n\nIf the customer uploaded vaccination files or images, they are available in the Dropbox folder Customer Uploads. If they still need to upload these documents, they should visit pupculturenyc.com/upload.\n\n\n\nIf there is an issue with this application, please contact joshmarcus85@gmail.com\n\n", filename]
+	yag.send(p_con.recipient_emails, subject, contents)
+	
+	### MESSAGE TO ANNOUNCE EMAILING DOCUMENT
+	print("\nEmailing a registration document from " + pet_name.title() + " " + 
+			last_name.title() + " to " + p_con.recipient_emails + "!")
+		### Post Status Message to Slack Channel ###
+	new_email_slack_msg = ('\nA new registration document for ' + pet_name.title() + ' ' + last_name.title() + ' has been emailed!')
+	slack.chat.post_message(p_con.slack_channel, new_email_slack_msg)
+	
+
+	
 '''	
 	#### Saving to Dropbox #####
 	print("\nSaving file to Dropbox ... ")
@@ -100,14 +125,14 @@ def email_it(row_number):
 	os.chdir('submitted_customer_files')
 				
 	f = open(cust_filename, 'rb')
-	response = client.put_file(('/' + last_name.title() + ', ' + pet_name.title() + ".docx"), 'f') 
+	response = client.put_file(('/' + last_name.title() + '_' + pet_name.title() + ".docx"), 'f') 
 	print 'uploaded ', response
 		
 	folder_metadata = client.metadata('/')
 	print 'metadata: ', folder_metadata
 		
-	f, metadata = client.get_file_and_metadata('/' + last_name.title() + ', ' + pet_name.title() + ".docx")
-	out = open('/' + last_name.title() + ', ' + pet_name.title() + ".docx", 'wb')
+	f, metadata = client.get_file_and_metadata('/' + last_name.title() + '_' + pet_name.title() + ".docx")
+	out = open('/' + last_name.title() + '_' + pet_name.title() + ".docx", 'wb')
 	out.write(f.read())
 	out.close()
 	print metadata
--- import_gsheet_by_row.py
+++ import_gsheet_by_row.py
@@ -5,7 +5,8 @@ from slacker import Slacker
 from mailmerge import MailMerge
 import private_config as p_con
 from datetime import datetime
-import create_docx as c_doc
+import get_reg as gr
+import email_it as e_it
 
 ### SLACK CLIENT ###
 from slacker import Slacker
@@ -35,7 +36,7 @@ reg = gc.open_by_url('https://docs.google.com/spreadsheets/d/1dYO0M9iBWVmOYcE8fO
 global worksheet
 worksheet = reg.get_worksheet(0)
 
-def import_gsheet_by_row(row_number):
+def import_gsheet_by_row_and_check(row_number):
 	
 	# docx Template File (pc Registration Form with MailMerge Fields)
 	template = os.path.join(this_dir, 'pcregtemplate.docx')
@@ -137,6 +138,102 @@ def import_gsheet_by_row(row_number):
 	# Receives another email address here for some reason. 
 	email_again = worksheet.acell('BG' + str(row_number)).value.strip()
 	
+	if last_name.title().strip() and first_name.title().strip() and pet_name.title().strip() in open('inputlog.txt').read():
+	#if any(s in line for s in test_strings):
+		print("\nRegistration for " + pet_name.title().strip() + " " + last_name.title().strip() + " found!")
+		return
+	# Need to make sure fields are not blank
+	elif not pet_name and not last_name and not first_name:
+		print("\n Aww man... looks like some of the fields are blank. I'm going to move on.")	
+		return
+	else:
+		print("\nCreating customer document for " + pet_name.title().strip() + 
+			" " + last_name.title().strip() + " ...")
+	document.merge(
+		last_name = last_name,
+		first_name = first_name,
+		address = street,
+		apartment = apt,
+		cross_streets = cross_streets,
+		city = city,
+		state = state, 
+		zip = zip_code,
+		home_phone = home_phone,
+		cell_phone = cell_phone,
+		business_phone = business_phone,
+		email_address = email_address,
+		reference = reference,
+		emergency_contacts = emergency_contacts,
+		membership = membership,
+		keys = keys,
+		pet_name = pet_name,
+		pet_nick = pet_nick,
+		breed = breed,
+		weight = weight,
+		sex = sex,
+		dob = dob,
+		color = color,
+		spayed = fixed,
+		how_long_owned = length_owned,
+		brand_food = brand_food,
+		times_fed = times_per_day_feed,
+		size_portion = serving_size,
+		allergies_digestive = allergies_digestive,
+		treats_ok = treats_ok,
+		fed_during_daycare = fed_during_daycare,
+		in_home_restrictions = home_restriction,
+		water_out = water_out,
+		dry_food = dry_food, 
+		leash_location = leash_location,
+		where_is_the_stuff = where_is_the_stuff,
+		where_is_the_other_stuff = where_is_the_other_stuff,
+		lights_locks = lights_locks,
+		quirks = helpful_info,
+		dog_allowed = dog_allowed,
+		behavior = behavior,
+		behavior_new_people = behavior_new_people,
+		behavior_leash = behavior_leash,
+		housebroken = housebroken,
+		last_vet = last_vet,
+		flea_prevention = flea_prevention,
+		medical_conditions = medical_conditions,
+		medication = medications,
+		ever_bitten = ever_attacked,
+		vet_name = vet_name,
+		vet_phone = vet_phone,
+		vet_address = vet_address,
+		vet_city = vet_city,
+		vet_state = vet_state,
+		date = today_date)
+		# Write completed document
+		# First, change directory to submitted_customer_files
+	print("Writing new customer file ...")
+	os.chdir('submitted_customer_files')
+	document.write(last_name.title().strip() + '_' + pet_name.title().strip() + '.docx')
+	os.chdir('..')
+	# Terminal Message to Confirm Success
+	new_reg_msg = "\nA registration docx form for " + pet_name.title().strip() + " " + last_name.title().strip() + " has been created!\t"
+	print(new_reg_msg)
+	'''
+	### Convert docx to pdf and store in folder called pdfs
+	# Call w2p here
+	file_to_convert = last_name.title() + '_' + pet_name.title() + '.docx'
+	converted_pdf = last_name.title() + '_' + pet_name.title() + '.pdf'
+	w2p.convx_to_pdf(file_to_convert, converted_pdf)
+	'''
+	### Post Slack Message to Update Channel
+	#slack.chat.post_message(p_con.slack_channel, new_reg_msg)
+	
+	##### LOGGING TO TEXT FILE #####
+	with open('inputlog.txt', 'a') as doglog:
+		doglog.write('\n' + last_name.title().strip() + ', ' + first_name.title().strip() + ' (Owner), ' + pet_name.title().strip() + ':  ' + str(today_date)) 
+	### Confirm Logging ###
+	print("Registration information for " + pet_name.title().strip() + " " + last_name.title().strip() + 
+				" has been logged to inputlog.txt!")
+		
+	e_it.email_it(row_number)
+	return
+		
 	'''
 	##### CHECK FOR EMPTY FIELDS #####
 	#### Check for empty fields in registration. Maybe picked the wrong row?
